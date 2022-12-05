Fix chunk predictions and refactor NeRF utils - Rewrite the code to chunk the whole pipeline instead of only on MLP - Initialize TF Variable to store the accumulated loss and gradients - Refactor the NeRF compile into multiple sub-function - Refactor nerf.utils function into NeRFUtils class - Test the updated codes

inference.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 import imageio
 
 from keras_nerf.model.nerf.nerf import NeRF
-from keras_nerf.model.nerf.utils import positional_encoding, encode_position_and_directions, render_image_depth, fine_hierarchical_sampling
 from keras_nerf.data.utils import get_focal_from_fov, pose_spherical
 from keras_nerf.data.rays import RaysGenerator
 
@@ -101,6 +100,9 @@ def main():
     else:
         nerf_predictions = tf.function(
             nerf.predict_and_render_images, reduce_retracing=True)
+
+    nerf.coarse.summary()
+    nerf.fine.summary()
 
     images = []
     depth = []

train_single.py
@@ -28,7 +28,7 @@ def main():
     parser.add_argument('--skip_layer', type=int, default=4)
 
     # NeRF Dataset Parameters
-    parser.add_argument('--img_wh', type=int, default=64)
+    parser.add_argument('--img_wh', type=int, default=512)
     parser.add_argument('--near', type=float, default=2.0)
     parser.add_argument('--far', type=float, default=6.0)
 
@@ -38,16 +38,34 @@ def main():
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--num_gpus', type=int, default=1)
-    parser.add_argument('--ray_chunks', type=int, default=1024)
+    parser.add_argument('--ray_chunks', type=int, default=2048)
     parser.add_argument('--eagerly', action='store_true')
 
     # NeRF Logging Parameters
     parser.add_argument('--model_dirs', type=str, default='model')
     parser.add_argument('--log_dir', type=str, default='logs')
     parser.add_argument('--log_freq', type=int, default=1)
+    parser.add_argument('--verbose', action='store_true')
 
     args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
     logging.info(args)
+
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(
+                logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
     # Load the data
     dataset_loader = DatasetLoader(args.data_dir)
@@ -119,6 +137,4 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
     main()